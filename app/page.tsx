6fc8779d0859ad35e5ab433fa1427b78fac0579d
Lea Grigat <leagrigat@gmail.com>
2024-04-19 12:24:31 +0200

    set up homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-// app/page.tsx
-'use client'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-export default function Page() {
+
+
+export default function Home() {
   return (
     <>
-  
-
-    <div className='flex justify-between '>
-   
+    <h1>Welcome to FlickSync 🎞️🍿</h1>
+    <div className="w-screen h-screen flex justify-center">
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="groupName" placeholder="Please enter a group name" />
+      <Button type="submit">Save</Button>
+    </div>
     </div>
     </>
   )
