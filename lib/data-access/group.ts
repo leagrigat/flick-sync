45c040aadc60c49b2a1b82975683ea3988dc9e8f
Lea Grigat <leagrigat@gmail.com>
2024-04-24 17:14:37 +0200

    chore: deleted unnecessary comments

--- lib/data-access/group.ts
+++ lib/data-access/group.ts
@@ -17,26 +17,3 @@ export async function createGroup(values: NewGroup){
         .returning({id: group.id})
     })
 }
-
-
-// export async function createCompany(
-//     values: NewCompany & { memberIds: string[] },
-//   ) {
-//     const { memberIds, ...newCompany } = values
-//     return db.transaction(async (tx) => {
-//       const [result] = await tx
-//         .insert(companies)
-//         .values(newCompany)
-//         .returning({ id: companies.id })
-  
-//       if (memberIds.length > 0) {
-//         await tx.insert(companiesToMembers).values(
-//           memberIds.map((memberId) => ({
-//             companyId: result.id,
-//             memberId: memberId,
-//           })),
-//         )
-//       }
-//       return result.id
-//     })
-//   }
